384bc82bd244251908b23e6e7a13b1ce67609d8c
chriskilinc <chriskilinc@gmail.com>
2018-06-29 14:17:10 +0200

    Added Default route for /api/artist/

--- src/routes/artist.js
+++ src/routes/artist.js
@@ -1,5 +1,10 @@
 const router = require('express').Router();
 
+router.get('/', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify('api/artist/id'));
+});
+
 router.get('/:id', (req, res) => {
   res.send('Artist' + req.params.id);
 });
