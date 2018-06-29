ca03531431b9f940abbcbeb3eff03199019bf0d8
chriskilinc <chriskilinc@gmail.com>
2018-06-29 15:10:07 +0200

    Implemented Artist Controller in Route

--- src/routes/artist.js
+++ src/routes/artist.js
@@ -1,12 +1,11 @@
 const router = require('express').Router();
+const controllerArtist = require('../controllers/artist');
 
 router.get('/', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
   res.send(JSON.stringify('api/artist/id'));
 });
 
-router.get('/:id', (req, res) => {
-  res.send('Artist' + req.params.id);
-});
+router.get('/:id', controllerArtist.getArtistFromMusicBrainz);
 
 module.exports = router;
