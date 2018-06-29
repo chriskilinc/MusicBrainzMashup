0fdcf838b8a218a0574c12738df90825c0b6745e
chriskilinc <chriskilinc@gmail.com>
2018-06-29 14:01:08 +0200

    Implemented Express and Routes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('hi');
+const port = process.env.PORT || 3000; //  Configures the application PORT to either the current enviorments port OR if enviorment port is not set, port '3000'
+const express = require('express');
+const routeArtist = require('./routes/artist');
+const routeDefault = require('./routes/default');
+const app = express();
+
+//  Root Route
+app.use('/', routeDefault);
+
+//  Artist by mbid
+app.use('/api/artist', routeArtist);
+
+app.listen(port, () => console.log(`Listening on port ${port}...`));
