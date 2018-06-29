3cc30fec2de8522585fa38e4273ef48dbe0e1eb9
chriskilinc <chriskilinc@gmail.com>
2018-06-29 14:17:29 +0200

    Implemented Api Route

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 const port = process.env.PORT || 3000; //  Configures the application PORT to either the current enviorments port OR if enviorment port is not set, port '3000'
 const express = require('express');
-const routeArtist = require('./routes/artist');
+const routeApi = require('./routes/api');
 const routeDefault = require('./routes/default');
+const routeArtist = require('./routes/artist');
 const app = express();
 
 //  Root Route
 app.use('/', routeDefault);
 
-//  Artist by mbid
+//  Api Route
+app.use('/api', routeApi);
+
+//  Artist Route
 app.use('/api/artist', routeArtist);
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
