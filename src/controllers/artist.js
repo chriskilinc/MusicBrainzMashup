de85ace11eceb1f37434e43d7e117e35c93b0334
chriskilinc <chriskilinc@gmail.com>
2018-07-01 14:31:22 +0200

    Minor structure changes

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -6,48 +6,8 @@ const { fetchCoverArt } = require('../utilities/coverArtArchive');
 async function getArtist(req, res) {
   const mbid = req.params.id;
   res.setHeader('Content-Type', 'application/json');
-  if (isUuid(mbid)) {
-    try {
-      //  Fetches Aritst from MusicBrains and Returns a Artist Object
-      let artistModel = await fetchArtistFromMusicBrainz(mbid);
 
-      //  Fetches Wikipedia and returns a short description
-      const artistDescription = await fetchDescFromWikipedia(
-        artistModel.wikipediaUrl
-      );
-      //  Fetches CoverArtArchive and returns Album Art for each Album in artistModel
-      const albumCoverArt = artistModel.albums.map(async album => {
-        let image = await fetchCoverArt(album.id);
-        return { ...album, image };
-      });
-
-      //    Without PROMISE ALL
-      // artist.description = artistDescription;
-      // let mapedAlbumCovers = await Promise.all([...albumCoverArt]);
-      // artist.albums = [...mapedAlbumCovers];
-
-      //    PROMISE ALL - Almost 1000ms faster than Without
-      await Promise.all([artistDescription, ...albumCoverArt]).then(res => {
-        artistModel.description = res[0];
-        artistModel.albums = [...res].slice(1);
-        delete artistModel.wikipediaUrl;
-      });
-
-      res.send(JSON.stringify(artistModel));
-    } catch (error) {
-      // console.log(error.response.data);
-      res
-        .status(500)
-        .send(
-          JSON.stringify({
-            error: {
-              message: 'Unexpected Error'
-            }
-          })
-        )
-        .end();
-    }
-  } else {
+  if (!isUuid(mbid)) {
     res.status(400).send(
       JSON.stringify({
         error: {
@@ -55,6 +15,46 @@ async function getArtist(req, res) {
         }
       })
     );
+    throw new Error('Parameter is not a Valid UUID');
+  }
+
+  try {
+    //  Fetches Aritst from MusicBrains and Returns a Artist Object
+    let artistModel = await fetchArtistFromMusicBrainz(mbid);
+
+    //  Fetches Wikipedia and returns a short description
+    const artistDescription = fetchDescFromWikipedia(artistModel.wikipediaUrl);
+    //  Fetches CoverArtArchive and returns Album Art for each Album in artistModel
+    const albumCoverArt = artistModel.albums.map(async album => {
+      let image = await fetchCoverArt(album.id);
+      return { ...album, image };
+    });
+
+    //    Without PROMISE ALL
+    // artist.description = artistDescription;
+    // let mapedAlbumCovers = await Promise.all([...albumCoverArt]);
+    // artist.albums = [...mapedAlbumCovers];
+
+    //    PROMISE ALL - Almost 1000ms faster than Without
+    await Promise.all([artistDescription, ...albumCoverArt]).then(res => {
+      artistModel.description = res[0];
+      artistModel.albums = [...res].slice(1);
+      delete artistModel.wikipediaUrl;
+    });
+
+    res.send(JSON.stringify(artistModel));
+  } catch (error) {
+    // console.log(error.response.data);
+    res
+      .status(500)
+      .send(
+        JSON.stringify({
+          error: {
+            message: 'Unexpected Error'
+          }
+        })
+      )
+      .end();
   }
 }
 module.exports = { getArtist };
